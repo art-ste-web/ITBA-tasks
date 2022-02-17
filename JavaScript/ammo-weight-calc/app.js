a2e0072ca36df2b5d190968f81dbc565ec7c2320
art.ste.web <art.ste.web@gmail.com>
2022-02-17 15:41:13 +0200

    fix js

--- JavaScript/ammo-weight-calc/app.js
+++ JavaScript/ammo-weight-calc/app.js
@@ -6,22 +6,21 @@ function calcAmmoMass() {
     showTotalMass(totalAmmoMass);
 
 }
-function showTotalMass(totalMassValue) {
-    const totalAmmoEl = document.querySelector('.total-ammo-mass');
-    totalAmmoEl.innerText = `${totalMassValue} г (${(totalMassValue/1000).toFixed(3)} кг)`;
-}
-
 
 function calcTotalMass(value, number) {
     let totalMassGram = value*number;
     return totalMassGram.toFixed(2);
 }
 
-
-
 function showOneAmmoValue(massValue) {
     const oneAmmoEl = document.querySelector('.one-ammo-mass');
     oneAmmoEl.innerText = `${massValue} г`;
+}
+
+
+function showTotalMass(totalMassValue) {
+    const totalAmmoEl = document.querySelector('.total-ammo-mass');
+    totalAmmoEl.innerText = `${totalMassValue} г (${(totalMassValue/1000).toFixed(3)} кг)`;
 }
 
 const calcForm = document.querySelector('.calc-container');
