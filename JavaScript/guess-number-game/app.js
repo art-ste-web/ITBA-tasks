89f193e226120144907f2dec4b66d4bd2d3500fb
art.ste.web <art.ste.web@gmail.com>
2022-02-21 16:33:08 +0200

    add check user num

--- JavaScript/guess-number-game/app.js
+++ JavaScript/guess-number-game/app.js
@@ -4,53 +4,69 @@
 function generateRandomNumber(startNum, endNum) {
     const randomNum = Math.random() * (endNum - startNum) + startNum;
     console.log(randomNum.toFixed(0));
-    return randomNum.toFixed(0);
+    return +randomNum.toFixed(0);
 }
 
 function gameStart() {
+    let randomNumber = null;
+    let settings = null;
     const startBtn = document.getElementById('start_game_btn');
+    //confirm setting and start game
     startBtn.addEventListener('click', ()=>{
         hideSettingsBlock();
         showGameBlock();
+        settings = getInitialSettings();
+        const startNum = +settings.startNumb;
+        const endNum = +settings.endNumb;
+        randomNumber = generateRandomNumber(startNum, endNum);
+        console.log(randomNumber);
+        showGameMessage(`Введіть число в діапазоні від ${startNum} до ${endNum}`);
     });
+    
     let count = 0;
-    let prevNum = [];
+    let checkedNums = [];
+
+    //check user number
     const checkNumBtn = document.getElementById('check_numb_btn');
     checkNumBtn.addEventListener('click', ()=>{
-        
         const userNumbInput = document.getElementById('enter_numb');
-        if(!userNumbInput.value) {
-            showGameMessage('Ви не ввели число! Введіть число.')
-            return
-        }
-        for(let i=0; i<prevNum.length; i++) {
-            if(prevNum[i]===+userNumbInput.value){
-                showGameMessage('Це число вже було...');
-                return
-            }
-        }
-        count++;
-        checkUserNumber(count, prevNum);
-        console.log(prevNum);
+        const userNumb = +userNumbInput.value;
+        checkUserNumber(count, userNumb, checkedNums, randomNumber, settings);
     })
 }
 
-function checkUserNumber(count, prevNum) {
+function checkUserNumber(count, userNumb, checkedNums, randomNumber, settings) {
     
-    const userNumbInput = document.getElementById('enter_numb');
-    // if(!userNumbInput.value) {
-    //     showGameMessage('Ви не ввели число! Введіть число.')
-    // }
-    prevNum.push(+userNumbInput.value);
-    console.log(userNumbInput.value);
-    const settings = getInitialSettings();
-    
-    
-    console.log(count, +settings.attemptsNumb);
-    if(count === +settings.attemptsNumb) {
-        showGameMessage('Нажаль Ви програли...');
-        count = 0;
+    if(!userNumb) {
+        showGameMessage('Ви не ввели число! Введіть число.');
+        return
     }
+    for(let i=0; i<checkedNums.length; i++) {
+        if(checkedNums[i]===userNumb){
+            showGameMessage('Це число вже було, введіть інше');
+            return
+        }
+    }
+    if(userNumb > randomNumber) {
+        showGameMessage(`Загадане число менше ${userNumb}`);
+        showNumbersList(`${String(checkedNums)} Кількість спроб: ${settings.attemptsNumb - count}`);
+    } else if(userNumb < randomNumber) {
+        showGameMessage(`Загадане число більше ${userNumb}`);
+        showNumbersList(`${String(checkedNums)} Кількість спроб: ${settings.attemptsNumb - count}`);
+    } else if(userNumb === randomNumber) {
+        showGameMessage(`Вітаємо! Ви вгадали число ${randomNumber}. Натисніть "Нова гра", щоб зіграти ще`);
+        showNumbersList(`${String(checkedNums)} Кількість спроб: ${settings.attemptsNumb - count}`);
+        return
+    } else if(count === +settings.attemptsNumb) {
+        showGameMessage('Нажаль Ви програли...');
+        count = 1;
+        return
+    }
+    
+    count++;
+    checkedNums.push(userNumb);
+    
+    console.log(checkedNums);
     
 }
 
@@ -73,6 +89,11 @@ function showGameMessage(message) {
     messageBlock.textContent = message;
 }
 
+function showNumbersList(numList) {
+    const numListEl = document.querySelector('.app-form__attempts-nums');
+    numListEl.innerText = `Ви вже спробували числа: ${numList}`;
+}
+
 function hideSettingsBlock() {
     const settingsBlock = document.querySelector('.app-form__initial-settings');
     settingsBlock.style.height = 0;
@@ -84,7 +105,7 @@ function showGameBlock() {
     const gameBlock = document.querySelector('.app-form__guess-number-block');
     gameBlock.style.opacity = 1;
 }
-getInitialSettings();
+//getInitialSettings();
 
-generateRandomNumber(10, 20);
+//generateRandomNumber(10, 20);
 gameStart()
